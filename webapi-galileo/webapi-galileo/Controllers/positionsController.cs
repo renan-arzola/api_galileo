1f210f06d246eb0706763578aa820d5aca0af522
Renán Arzola <renan_arzola@hotmail.com>
2017-06-25 20:02:58 -0500

    Version funcional, GAL_1680

--- webapi-galileo/webapi-galileo/Controllers/positionsController.cs
+++ webapi-galileo/webapi-galileo/Controllers/positionsController.cs
@@ -6,27 +6,28 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using webapi_galileo;
 
 namespace webapi_galileo.Controllers
 {
-    public class LastPositions : ApiController
+    public class positionsController : ApiController
     {
         private galileo_dbEntities db = new galileo_dbEntities();
 
-        // GET: api/LastPositions
+        // GET: api/positions
         public IQueryable<GAL_1680> GetGAL_1680()
         {
             return db.GAL_1680;
         }
 
-        // GET: api/LastPositions/5
+        // GET: api/positions/5
         [ResponseType(typeof(GAL_1680))]
-        public IHttpActionResult GetGAL_1680(long id)
+        public async Task<IHttpActionResult> GetGAL_1680(long id)
         {
-            GAL_1680 gAL_1680 = db.GAL_1680.Find(id);
+            GAL_1680 gAL_1680 = await db.GAL_1680.FindAsync(id);
             if (gAL_1680 == null)
             {
                 return NotFound();
@@ -35,9 +36,9 @@ namespace webapi_galileo.Controllers
             return Ok(gAL_1680);
         }
 
-        // PUT: api/LastPositions/5
+        // PUT: api/positions/5
         [ResponseType(typeof(void))]
-        public IHttpActionResult PutGAL_1680(long id, GAL_1680 gAL_1680)
+        public async Task<IHttpActionResult> PutGAL_1680(long id, GAL_1680 gAL_1680)
         {
             if (!ModelState.IsValid)
             {
@@ -53,7 +54,7 @@ namespace webapi_galileo.Controllers
 
             try
             {
-                db.SaveChanges();
+                await db.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -70,9 +71,9 @@ namespace webapi_galileo.Controllers
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // POST: api/LastPositions
+        // POST: api/positions
         [ResponseType(typeof(GAL_1680))]
-        public IHttpActionResult PostGAL_1680(GAL_1680 gAL_1680)
+        public async Task<IHttpActionResult> PostGAL_1680(GAL_1680 gAL_1680)
         {
             if (!ModelState.IsValid)
             {
@@ -80,23 +81,23 @@ namespace webapi_galileo.Controllers
             }
 
             db.GAL_1680.Add(gAL_1680);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
 
             return CreatedAtRoute("DefaultApi", new { id = gAL_1680.cod_event }, gAL_1680);
         }
 
-        // DELETE: api/LastPositions/5
+        // DELETE: api/positions/5
         [ResponseType(typeof(GAL_1680))]
-        public IHttpActionResult DeleteGAL_1680(long id)
+        public async Task<IHttpActionResult> DeleteGAL_1680(long id)
         {
-            GAL_1680 gAL_1680 = db.GAL_1680.Find(id);
+            GAL_1680 gAL_1680 = await db.GAL_1680.FindAsync(id);
             if (gAL_1680 == null)
             {
                 return NotFound();
             }
 
             db.GAL_1680.Remove(gAL_1680);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
 
             return Ok(gAL_1680);
         }
